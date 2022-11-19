3b91fbddbfe1f35e9d4e63f25fc7b157ff35d167
muhamet111 <muhamet4460@gmail.com>
2022-11-19 17:29:38 +0000

    Vendor feature done

--- src/test/java/com/cydeo/pages/VendorsPage.java
+++ src/test/java/com/cydeo/pages/VendorsPage.java
@@ -12,7 +12,7 @@ public class VendorsPage {
     }
 
     @FindBy(xpath = "//div[2]/ul[1]/li[9]/a/span")
-    public WebElement PurchasesBtn;
+    public WebElement PurchasesBtnn;
 
     @FindBy(xpath = "//div[9]/ul[1]/li[3]/a/span")
     public WebElement vendorsBtn;
@@ -21,10 +21,13 @@ public class VendorsPage {
     public WebElement vendorCreateBtn;
 
     @FindBy(xpath = "//input[@class=\"o_field_char o_field_widget o_input o_required_modifier\"]")
-    public WebElement VendorNameField;
+    public WebElement vendorName;
 
     @FindBy(xpath = "//button[@accesskey=\"s\"]")
     public WebElement vendorSaveBtn;
+
+    @FindBy(xpath = "//div/div[1]/div/div[3]/h1/span")
+    public WebElement VendorID;
 
     @FindBy(xpath = "//button[@accesskey=\"a\"]")
     public WebElement VendorEditBtn;
@@ -35,17 +38,20 @@ public class VendorsPage {
     @FindBy(xpath = "//div[2]/div/div[3]/ul/li[1]/a")
     public WebElement vendorDeleteBtn;
 
-    @FindBy(xpath = "//h4[@class=\"modal-title\"]")
+    @FindBy(xpath = "/html/body/div[5]/div/div/div[1]/h4")
     public WebElement ConfirmationMessage;
 
     @FindBy(xpath = "//div[5]/div/div/div[3]/button[1]")
-    public WebElement ConfirmationOkBtn;
+    public WebElement ConfirmationOkBttn;
 
     @FindBy(xpath = "//div[2]/div[1]/div[3]/div[3]/button[1]")
     public WebElement vendorKanbanBtn;
 
     @FindBy(xpath = "//button[@class=\"btn btn-icon fa fa-lg fa-list-ul o_cp_switch_list\"]")
     public WebElement vendorListBtn;
+
+    @FindBy(xpath = "/html/body/div[1]/div[2]/div[2]")
+    public WebElement kanbanlistwiev;
 
     @FindBy(xpath = "//button[@class=\"btn btn-default btn-sm o_form_button_cancel\"]")
     public WebElement vendorDiscardBtn;
@@ -56,20 +62,21 @@ public class VendorsPage {
     @FindBy(xpath = "//button[@class=\"btn btn-sm btn-primary\"]")
     public WebElement WarningOKBtn;
 
-    @FindBy(xpath = "//button[@aria-label=\"Previous\"]")
+    @FindBy(xpath = "//div[@class=\"oe_tooltip_string\"]")
+    public WebElement DiscardMessage;
+
+
+    @FindBy(xpath = "//button[@class=\"fa fa-chevron-left btn btn-icon o_pager_previous\"]")
     public WebElement LeftButton;
 
-    @FindBy(xpath = "//button[@aria-label=\"Next\"]")
+    @FindBy(xpath = "//button[@class=\"fa fa-chevron-right btn btn-icon o_pager_next\"]")
     public WebElement RightButton;
 
+   @FindBy(xpath = "//span[@class=\"o_pager_value\"]")
+    public WebElement RightBuutonPage;
 
-
-
-
-
-
-
-
+   @FindBy(xpath = "//div[3]/div[2]/div/span[1]/span[1]")
+    public WebElement LeftbuttonPAge;
 
 
 
